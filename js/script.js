0c55b63583ce7a1fa1dcb6ad43ff97bd91db0853
Aleksey Kerkin <aleksey.kerkin@yandex.ru>
2024-12-09 18:24:38 +0500

    Add function to get human player's choice
    - `getHumanChoice()`: Prompts the user to input a number corresponding
     to their choice (1 -> "rock", 2 -> "paper", 3 -> "scissors").
      - Converts the user's input to an integer.
      - Maps the integer to the corresponding choice.
      - Returns "Wrong input" if the input is not 1, 2, or 3.

--- js/script.js
+++ js/script.js
@@ -1,9 +1,11 @@
-// Create a new function named getComputerChoice.
-// Write the code so that getComputerChoice will randomly return one of the following string values: “rock”, “paper” or “scissors”.
-// Note: Some learners might think that arrays are necessary for this problem, but they aren’t. If you’re unfamiliar with arrays, don’t worry - they’ll be covered later in the curriculum.
-// Test that your function returns what you expect using console.log or the browser developer tools before advancing to the next step.
+// 1. Create a new function named getComputerChoice.
 
-// Get the random integer, that gives us a random number from 0 to value that you set in the "max" parameter
+// 2. Write the code so that getComputerChoice will randomly return one of the following string values: “rock”, “paper” or “scissors”.
+// Note: Some learners might think that arrays are necessary for this problem, but they aren’t. If you’re unfamiliar with arrays, don’t worry - they’ll be covered later in the curriculum.
+
+// 3. Test that your function returns what you expect using console.log or the browser developer tools before advancing to the next step.
+
+// Function that generates a random integer between 0 (inclusive) and max (exclusive)
 function getRandomInt(max) {
   return Math.floor(Math.random() * max);
 }
@@ -25,3 +27,25 @@ function getComputerChoice(int) {
 }
 
 console.log(getComputerChoice(randomInt));
+
+// 1. Create a new function named getHumanChoice.
+function getHumanChoice() {
+  const int = parseInt(
+    prompt(`1 -> "rock"\n2 -> "paper"\n3 -> "scissors"\nEnter a number: `)
+  );
+  if (int === 1) {
+    return "rock";
+  } else if (int === 2) {
+    return "paper";
+  } else if (int === 3) {
+    return "scissors";
+  } else {
+    return "Wrong input";
+  }
+}
+// 2. Write the code so that getHumanChoice will return one of the valid choices depending on what the user inputs.
+
+// Hint: Use the prompt method to get the user’s input.
+// Test what your function returns by using console.log.
+
+console.log(getHumanChoice());
